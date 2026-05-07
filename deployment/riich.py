036f6308e1e8f6fdcf6e9fe98929e26df1f00649
maitreverge_fedora <104826504+maitreverge@users.noreply.github.com>
2026-05-07 14:10:05 +0200

    UPDATE: Wallet manager is working

--- deployment/riich.py
+++ deployment/riich.py
@@ -6,19 +6,19 @@ from rich import box
 from rich.live import Live
 from rich.layout import Layout
 import readchar
-import json
-
-from typing import Any
 
 from dotenv import load_dotenv
 import os
 import sys
+from web3 import Web3
+from typing import Any
 
 from eth_account import Account
 
 load_dotenv()
 
-WALLETS_JSON = os.getenv("WALLETS_JSON")
+WALLETS_FILE = os.getenv("WALLETS_FILE")
+SEPOLIA_ENTRYPOINT = os.getenv("SEPOLIA_ENTRYPOINT")
 
 console = Console()
 
@@ -26,54 +26,50 @@ class Wallet():
     def __init__(self, key: str | None = None) -> None:
         
         if not key:
-            self.account = Account.create('KEYSMASH FJAFJKLDSKF7JKFDJ 1530')
+            self.account = Account.create()
         else:
             self.account = Account.from_key(key)
+        
+        self._entrypoint_obj = Web3(Web3.HTTPProvider(SEPOLIA_ENTRYPOINT))
+        
+        self._sepolia_balance_wei: int = self._fetch_balance()
+
+    def _fetch_balance(self) -> int:
+        return self._entrypoint_obj.eth.get_balance(self.get_public_key())
     
-    def get_private_key(self) -> str:
-        return self.account.key.hex()
+
+    def refresh_balance(self) -> None:
+        self._sepolia_balance_wei = self._fetch_balance()
+
+    @property
+    def sepolia_balance(self) -> float:
+        return round((self._sepolia_balance_wei / 10**18), 4)
     
     def get_public_key(self) -> str:
         return self.account.address
     
+    def get_private_key(self) -> str:
+        return self.account.key.hex()
+    
 
-# --- Mock data ---
-WALLETS = [
-    {
-        "name": "Cranberry",
-        "number": 0,
-        "address": "0x2hd73dF9aB3c...",
-        "balance": "0.012 ETH",
-    },
-    {
-        "name": "Banana",
-        "number": 1,
-        "address": "0x9chdu3aA1D2e...",
-        "balance": "0.047 ETH",
-    },
-    {
-        "name": "Kiwi",
-        "number": 2,
-        "address": "0xcKdH63bB9f0a...",
-        "balance": "0.003 ETH",
-    },
-]
-
+    
 
 def create_wallet(wallets: list[Wallet]):
     new_wallet = Wallet()
 
-    if WALLETS_JSON is None:
-        return "[red]WALLETS_JSON is not configured.[/red]"
+    if WALLETS_FILE is None:
+        return "[red]WALLETS_FILE is not configured.[/red]"
 
-    with open(WALLETS_JSON, "a") as wallet_file:
+    with open(WALLETS_FILE, "a") as wallet_file:
         wallet_file.write(f"{new_wallet.get_private_key()}\n")
     wallets.append(new_wallet)
-    return "[green]✓ Wallet Created.[/green]"
+    return "[bold green]✓ Wallet Created.[/bold green]"
 
 
-def action_private_key():
-    return "[yellow]⚠  Private key (mock): 0xDEADBEEF...CAFEBABE[/yellow]"
+def refresh_balance(wallets: list[Wallet]):
+    for wallet in wallets:
+        wallet.refresh_balance()
+    return "[bold green]✓ Wallets balance refreshed [/bold green]"
 
 
 def build_table(wallets: list[Wallet]) -> Table:
@@ -87,14 +83,15 @@ def build_table(wallets: list[Wallet]) -> Table:
     table.add_column("Number", style="dim", min_width=8, justify="center")
     table.add_column("Public Address", style="cyan", min_width=20, justify="center")
     table.add_column(
-        "Solde (SepoliaETH)", style="green", min_width=18, justify="right"
+        "Balance (SepoliaETH)", style="green", min_width=18, justify="center"
     )
 
     if len(wallets) == 0:
         table.add_row("", "[bold red]NO WALLET AVAILABLE. SELECT 1 TO CREATE A WALLET.[/bold red]", "")
     else:
         for i, w in enumerate(wallets):
-            table.add_row(str(i), w.get_public_key(), str(0))
+            # table.add_row(str(i), w.get_public_key(), str(0))
+            table.add_row(str(i), w.get_public_key(), str(w.sepolia_balance))
     return table
 
 
@@ -119,7 +116,7 @@ def build_layout(wallets, input_buffer: str = "", status_msg: str = "") -> Layou
     menu_text.append(". Create a wallet\n", style="white")
     menu_text.append("  2", style="bold yellow")
     menu_text.append(
-        ". Access private key of selected wallet\n", style="white"
+        ". Refresh Wallets balances\n", style="white"
     )
     menu_text.append("  3", style="bold yellow")
     menu_text.append(". Quit Program\n\n", style="white")
@@ -171,13 +168,13 @@ def main() -> None:
     # Buffer for user prompt
     buf = ""
     try:
-        wallets = load_wallets(WALLETS_JSON)
+        wallets = load_wallets(WALLETS_FILE)
     except Exception as e:
-        print(f"Error while loading wallets from {WALLETS_JSON}.\nError= {e}\nAborting")
+        print(f"Error while loading wallets from {WALLETS_FILE}.\nError= {e}\nAborting")
         sys.exit(1)
     
     with Live(
-        build_layout(wallets), console=console, screen=True, refresh_per_second=5
+        build_layout(wallets), console=console, screen=True, refresh_per_second=10
     ) as live:
         while True:
             live.update(build_layout(wallets, buf, status))
@@ -192,7 +189,7 @@ def main() -> None:
                 if cmd == "1":
                     status = create_wallet(wallets)
                 elif cmd == "2":
-                    status = action_private_key()
+                    status = refresh_balance(wallets)
                 elif cmd == "3":
                     break
                 else:
